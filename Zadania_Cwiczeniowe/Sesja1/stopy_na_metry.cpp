a3975ce62d4f8b273689bbfd3cc5da25272bdce6
Slim <93072451+SlimMojka@users.noreply.github.com>
2026-02-18 02:04:32 +0100

    Zadania z ksiazki Opus Magnum C++

---
+++ Zadania_Cwiczeniowe/Sesja1/stopy_na_metry.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float h_stopy;
+    float h_metry;
+
+    cout << "\n";
+    cout << "Podaj wysokosc w stopach: ";
+    cin >> h_stopy;
+
+    h_metry = ( h_stopy * 30.48 ) / 100.0;
+
+    cout << h_stopy << " stopy to: " << h_metry << " metra/metrow." << endl;
+
+    return 0;
+
+}
